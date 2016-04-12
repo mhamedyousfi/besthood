9499ae2d8bb537c333202600a056aec33ed48a68
mhamed <mhamed.yousfi@esprit.tn>
2016-04-12 22:01:49 +0100

    geolocation modification 4

--- modules/claims/client/controllers/claims.client.controller.js
+++ modules/claims/client/controllers/claims.client.controller.js
@@ -22,10 +22,10 @@
       navigator.geolocation.getCurrentPosition(function(position){
         $scope.$apply(function(){
 
-          vm.claim.lat = position.coords.latitude;
+          vm.lat = position.coords.latitude;
           $scope.lat = position.coords.latitude;
           vm.claim.lon = position.coords.longitude;
-          $scope.claim.lon = position.coords.longitude;
+          $scope.lon = position.coords.longitude;
         });
       });
     }
