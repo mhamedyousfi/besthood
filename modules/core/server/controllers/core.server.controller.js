83c340a7e8d8aea277994c20a543d6a142126552
khlech2008 <khlech2008@gmail.com>
2016-04-13 07:42:01 +0200

    statistique

--- modules/core/server/controllers/core.server.controller.js
+++ modules/core/server/controllers/core.server.controller.js
@@ -15,6 +15,7 @@ exports.renderIndex = function (req, res) {
       username: validator.escape(req.user.username),
       created: req.user.created.toString(),
       roles: req.user.roles,
+      comunity: req.user.comunity,
       profileImageURL: req.user.profileImageURL,
       email: validator.escape(req.user.email),
       lastName: validator.escape(req.user.lastName),
@@ -26,6 +27,7 @@ exports.renderIndex = function (req, res) {
   res.render('modules/core/server/views/index', {
     user: safeUserObject
   });
+
 };
 
 /**
