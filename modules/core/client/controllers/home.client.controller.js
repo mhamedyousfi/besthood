3cace8299b9baabc322bcda05c0148d0a348aa12
FerdawsGmar <ferdaws.gmar@esprit.tn>
2016-04-19 19:35:14 +0100

    update weather api ferdaws2

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,8 +1,23 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-  function ($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication','weatherService',
+  function ($scope, Authentication,weatherService) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+
+    function fetchWeather(place) {
+      weatherService.getWeather(place).then(function(data){
+        $scope.place = data;
+
+      });
+    }
+
+
+    fetchWeather('84105');
+    $scope.findWeather = function(place) {
+      $scope.place = '';
+      fetchWeather(place);
+    };
   }
+
 ]);
